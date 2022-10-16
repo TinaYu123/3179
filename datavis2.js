351d9b7870da32cc841729e6ded9f4f24b77f176
TinaYu123 <111409412+TinaYu123@users.noreply.github.com>
2022-10-17 00:08:38 +1100

    Add files via upload

---
+++ datavis2.js
@@ -0,0 +1,35 @@
+var vg_1 = "mean-years-of-schooling.vg.json";
+vegaEmbed("#choropleth", vg_1).then(function(result) {
+// Access the Vega view instance
+//(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_2 = "learning-outcomes.vg.json";
+vegaEmbed("#bubble", vg_2).then(function(result) {
+// Access the Vega view instance
+//(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_3 = "donut.vg.json";
+vegaEmbed("#donut", vg_3).then(function(result) {
+// Access the Vega view instance
+//(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_4 = "linechart.vg.json";
+vegaEmbed("#line", vg_4).then(function(result) {
+// Access the Vega view instance
+//(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_5 = "bar.vg.json";
+vegaEmbed("#bar", vg_5).then(function(result) {
+// Access the Vega view instance
+//(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_6 = "bar2.vg.json";
+vegaEmbed("#bar2", vg_6).then(function(result) {
+// Access the Vega view instance
+//(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
